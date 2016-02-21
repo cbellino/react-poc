3245c9e949bfb16af2125f84411df27dd4f71f1e
Colin Bellino <bellino.colin@gmail.com>
2016-02-21 19:52:37 +0100

    Fix unit tests

--- preprocessor.js
+++ preprocessor.js
@@ -1,11 +1,14 @@
-const babelJest = require('babel-jest')
-const webpackAlias = require('jest-webpack-alias')
+const babel = require('babel-core');
+// const babelJest = require('babel-jest');
+// const webpackAlias = require('jest-webpack-alias');
 
 module.exports = {
   process: function (src, filename) {
+    // Ignore all files within node_modules
     if (filename.indexOf('node_modules') === -1) {
-      src = babelJest.process(src, filename)
-      src = webpackAlias.process(src, filename)
+      // src = babelJest.process(src, filename);
+      // src = webpackAlias.process(src, filename);
+      return babel.transform(src, { filename: filename }).code;
     }
     return src
   }
